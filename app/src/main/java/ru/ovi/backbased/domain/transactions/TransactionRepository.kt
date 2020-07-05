0e1d2bde2fbb30aed0de7f6e82905d6bb9c5e54e
ovitaliy <ovitaliy.rudenko@gmail.com>
2020-07-05 16:39:07 +0300

    Remove unused classes

--- app/src/main/java/ru/ovi/backbased/domain/transactions/TransactionRepository.kt
+++
@@ -1,7 +0,0 @@
-package ru.ovi.backbased.domain.transactions
-
-interface TransactionRepository {
-
-    suspend fun getTransactions(): TransactionListModel
-    suspend fun getTransaction(ref: String): TransactionModel
-}
